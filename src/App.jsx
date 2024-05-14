0d59fe77b3bb79d1a6af58e571a392d048ea121e
jingenito <jingenito1530@gmail.com>
2024-05-13 22:16:15 -0400

    Fixed an issue where the gradient circle was cusing the background to cutoff near the boundaries. Also made the page more responsive for mobile views

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import './App.css';
 import Home from './components/home/Home.jsx';
 
 const App = () => {
   const [cursorPos, setCursorPos] = useState({ x: -100, y: -100 });
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
   const debounce = (func, wait) => {
     let timeout;
@@ -18,11 +19,22 @@ const App = () => {
     setCursorPos({ x: event.clientX, y: event.clientY });
   }, 10), []); // debounce time 10ms
 
+  const updateIsMobile = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', updateIsMobile);
+    return () => {
+      window.removeEventListener('resize', updateIsMobile);
+    };
+  }, []);
+
   return (
-    <div className="App" onMouseMove={handleMouseMove}>
-      <div
+    <div className="App" onMouseMove={!isMobile ? handleMouseMove : null} style={{ minHeight: '100vh' }}>
+      {!isMobile && (<div
         style={{
-          position: 'absolute',
+          position: 'fixed',
           top: cursorPos.y - 400, // Center the gradient on the cursor
           left: cursorPos.x - 400,
           width: '800px',
@@ -30,9 +42,10 @@ const App = () => {
           borderRadius: '50%',
           background: 'radial-gradient(circle, rgba(29, 78, 216, 0.15), transparent 80%)',
           pointerEvents: 'none', // Ensure clicks can pass through this element
+          zIndex: 1, // Ensure the gradient is above other content
         }}
       >
-      </div>
+      </div>)}
       <Home />
     </div>
   );
